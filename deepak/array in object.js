05214f7a7adc23728cb6fd590481ddfcbc8db51d
deepakraghuwansh <draghuwanshi724@gmail.com>
2023-04-12 18:04:55 +0530

    same change by deepak

---
+++ deepak/array in object.js
@@ -0,0 +1,4 @@
+var data=[{name:"rohit",age:21},{name:"deepak",age:21},{name:"ayush",age:24}];
+
+console.log(data[2].name)   // deepak print hoga //
+console.log(data)           // pura data print hoga //
