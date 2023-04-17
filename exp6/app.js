3fe6aab5c8fc46502f93938df61e39613cd5da41
jamesjoel <jamesjoelj@gmail.com>
2023-04-17 17:57:23 +0530

    ejs in node

---
+++ exp6/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname+"/assets"));
+
+app.set("view engine", "ejs");
+
+// app.set("views", "demo");
+
+
+app.get("/", (req, res)=>{
+    var data = [
+        {
+            name : "amar",
+            age : 24, 
+            city : "indore"
+        },
+        {
+            name : "vijay",
+            age : 21,
+            city : "mumbai"
+        },
+        {
+            name : "jaya",
+            age : 25,
+            city : "pune"
+        }
+    ];
+    var obj = { data };
+    
+    res.render("home", obj);
+})
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log("server running ", port);
+})
---
+++ exp6/demo.js
@@ -0,0 +1,6 @@
+var data = [10, 7, 5, 4, 8, 12, 6, 3];
+
+data.forEach((a, b)=>{
+    console.log(a);
+})
+
