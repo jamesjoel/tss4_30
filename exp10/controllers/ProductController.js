c8e8d0716999898239288dc7ae0c1f477dfd94f5
jamesjoel <jamesjoelj@gmail.com>
2023-05-02 18:03:39 +0530

    routes and crud

--- exp10/controllers/ProductController.js
+++ exp10/controllers/ProductController.js
@@ -15,4 +15,35 @@ routes.get("/view", async (req, res)=>{
     var obj = {result};
     res.render("pro_view", obj);
 })
+
+
+routes.get("/detail/:x", async (req, res)=>{
+    var a = req.params.x;
+
+    var result = await Product.find({ _id : a });
+    // allways getting single/one data only
+
+    var obj = { result : result[0]};
+    res.render("detail", obj);
+})
+routes.get("/edit/:x", async (req, res)=>{
+    var a = req.params.x;
+    var result = await Product.find({ _id : a });
+    var obj = { result : result[0]};
+    res.render("edit", obj);
+})
+routes.get("/delete/:x", async (req, res)=>{
+    var a = req.params.x;
+    
+    await Product.deleteMany({_id : a})
+    res.redirect("/product/view");
+})
+routes.post("/update/:x", async(req, res)=>{
+    var a = req.params.x;
+    await Product.updateMany({_id : a }, req.body);
+    res.redirect("/product/view");
+
+})
+
+
 module.exports = routes;
