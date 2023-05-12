36cdc5e5951f205d4091e03335cb03f187ad04e9
jamesjoel <jamesjoelj@gmail.com>
2023-05-12 18:09:37 +0530

    useState

--- react4/src/Contact.js
+++ react4/src/Contact.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Contact = () => {
+
+  // let [a, setA] = useState("");
+  // let [b, setB] = useState("");
+
+  let [user, setUser] = useState({ name : "", email : "" })
+
+
+  let show = ()=>{
+    console.log(user);
+  }
   return (
-    <div>Contact</div>
+    <>
+    <br />
+    <br />
+      <input type='text' value={user.name} onChange={(e)=> setUser({...user, name : e.target.value})}/>
+      <br />
+      <br />
+      <input type='text' value={user.email} onChange={(e)=>setUser({...user, email : e.target.value})} />
+      <br />
+      <button onClick={show}>OK</button>
+    </>
   )
 }
 
