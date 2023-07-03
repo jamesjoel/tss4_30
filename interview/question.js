401d5611c632f62d7ee558bd7188111a61c06a7f
jamesjoel <jamesjoelj@gmail.com>
2023-07-03 17:36:44 +0530

    interview question

---
+++ interview/question.js
@@ -0,0 +1,63 @@
+/*
+JavaScript 
+    1. what is JavaScript, and its versions ?
+    2. what variables and diff bwt let, const and var ?
+    3. types of variables. 
+    4. all datatypes
+    5. what is object?
+    6. what is array ?
+    7. destructuring ?
+    8. sperad ope
+    9. rest parameter
+    10. what is function and arrow function
+    11. what is callback function
+    12. promise
+    13. async and await
+    14. hoisting
+    15. Symbol() datatype.
+    16. Heigher Order function (forEach, map, filter, reduce)
+    17. diff bwt == and ===?
+    18. what is template literls
+
+NodeJS
+    1. What is NodeJS and History
+    2. REPL in Node.
+    3. what is modules in Node
+    4. NPM
+    5. require() and module.exports
+    6. what is ExpressJS and Where to use.
+    7. Detail Routing.
+    8. parameterized URL.
+
+MongoDB
+    1. How to Connect
+    2. what is mongoose (ORM -- Object Resoruce Model)
+    3. CRUD
+    4. import and export
+    5. how to live connect to mongodb.
+
+ReactJS
+    1. what is ReactJS and History
+    2. functional Component and Class Component.
+    3. what is State in React.
+    4. What is Props.
+    5. What is Hooks in react.
+    6. how to use --- useEffect hook.
+    7. form handling in React.
+    8. DOM events in React.
+    9. Routing in React (react-roter-dom)
+    10. formik and yup.
+    11. Network request in React.
+    12. controlled and un-controlled form element.
+    13. What is Redux.
+    14. explain --- Store, Reducers, actions and State.
+    15. What is Redux Thunk.
+    16. listing in React (.map())
+    17. self calling function
+
+
+
+
+    
+
+*/
